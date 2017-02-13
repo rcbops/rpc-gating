c5e50d20641ef4ca4a5cb19b15dcc83480824b15
Melissa Kam <melissa.kam@rackspace.com>
2017-02-13 14:23:32 -0600

    Parameterizing tempest for both AIO and multi-node AIO

--- pipeline-steps/tempest.groovy
+++ pipeline-steps/tempest.groovy
@@ -5,11 +5,11 @@ def tempest_install(){
   )
 }
 
-def tempest_run(Map args){
+def tempest_run(Map args) {
   withEnv(["RUN_TEMPEST_OPTS=${env.RUN_TEMPEST_OPTS}"]){
     sh """#!/bin/bash
-      utility_container="\$(lxc-ls |grep -m1 utility)"
-      lxc-attach \
+      utility_container="\$(${args.wrapper} lxc-ls |grep -m1 utility)"
+      ${args.wrapper} lxc-attach \
         --keep-env \
         -n \$utility_container \
         -- /opt/openstack_tempest_gate.sh \
@@ -18,56 +18,39 @@ def tempest_run(Map args){
   }
 }
 
-def multi_node_tempest_run(Map args){
-  withEnv(["RUN_TEMPEST_OPTS=${env.RUN_TEMPEST_OPTS}"]) {
-    sh """#!/bin/bash
-      utility_container="\$(sudo ssh -T -oStrictHostKeyChecking=no infra1 lxc-ls |grep -m1 utility)"
-      sudo ssh -T -oStrictHostKeyChecking=no infra1 lxc-attach \
-        --keep-env
-        -n \$utility_container \
-        -- /opt/openstack_tempest_gate.sh \
-        ${env.TEMPEST_TEST_SETS}
-  """
-  }
-}
 
 /* if tempest install fails, don't bother trying to run or collect test results
- * however if runnig fails, we should still collect the failed results
+ * however if running fails, we should still collect the failed results
  */
-def tempest(){
+def tempest(Map args){
+  if (args != null && args.containsKey("vm")) {
+    wrapper = "sudo ssh -T -oStrictHostKeyChecking=no ${args.vm} "
+    copy_cmd = "scp -o StrictHostKeyChecking=no -p  -r infra1:"
+  } else{
+    wrapper = ""
+    copy_cmd = "cp -p "
+  }
   common.conditionalStage(
-    stage_name: "Tempest",
+    stage_name: "Install Tempest",
     stage: {
       tempest_install()
+    }
+  )
+  common.conditionalStage(
+    stage_name: "Tempest Tests",
+    stage: {
       try{
-        tempest_run()
+        tempest_run(wrapper: wrapper)
       } catch (e){
         print(e)
         throw(e)
       } finally{
-        sh "rm *tempest*.xml; cp /openstack/log/*utility*/**/*tempest*.xml . ||:"
+        sh "rm -f *tempest*.xml; ${copy_cmd}/openstack/log/*utility*/**/*tempest*.xml . ||:"
         junit allowEmptyResults: true, testResults: '*tempest*.xml'
       } //finally
     } //stage
   ) //conditionalStage
 } //func
 
-def multi_node_tempest(){
-  common.conditionalStage(
-    stage_name: "Tempest",
-    stage: {
-      tempest_install()
-      try{
-        multi_node_tempest_run()
-      } catch (e){
-        print(e)
-        throw(e)
-      } finally{
-        sh "rm *tempest*.xml; scp -o StrictHostKeyChecking=no -r infra1:/openstack/log/*utility*/**/*tempest*.xml . ||:"
-        junit allowEmptyResults: true, testResults: '*tempest*.xml'
-      } //finally
-    } //stage
-  ) //conditionalStage
-} //func
 
 return this;
