fcda2883bc60d40e7d0366562f5f8df03d6e23e4
Hugh Saunders <hugh@wherenow.org>
2017-02-16 13:59:40 +0000

    Add Webhook Proxy
    This commit adds a job for configurting a webhook proxy.
    
    * Boots an instance and attaches it as a slave
    * Configures sshd and firewall on the instance via ansible
    * Drops upstart config and ssh key on master
    * Prompts user to install upstart service as the jenkins user doesn't
      have sudo.
    
    Connects rcbops/u-suk-dev#1080

---
+++ pipeline-steps/webhooks.groovy
@@ -0,0 +1,75 @@
+def webhooks(){
+  instance_name = "WEBHOOK-PROXY"
+  pubCloudSlave.getPubCloudSlave(instance_name: instance_name)
+  common.conditionalStage(
+    stage_name: "Webhooks",
+    stage:{
+      withCredentials([
+        file(
+          credentialsId: 'id_rsa_cloud10_jenkins_file',
+          variable: 'jenkins_ssh_privkey'
+        ),
+        usernamePassword(
+          credentialsId: "github_webhook_proxy_basic_auth",
+          usernameVariable: "webhookproxy_user",
+          passwordVariable: "webhookproxy_pass"
+        )
+      ]){
+        dir('rpc-gating/playbooks'){
+          common.venvPlaybook(
+            playbooks: [
+              "webhooks.yml"
+            ],
+            args: [
+              "-i inventory",
+              "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\""
+            ],
+            vars: [
+              WORKSPACE: "${env.WORKSPACE}",
+              webhookproxy_user: "${env.webhookproxy_user}",
+              webhookproxy_pass: "${env.webhookproxy_pass}"
+            ]
+          ) //venvPlaybook
+        } //dir
+      } //withCredentials
+      ip = readFile file: "instance_address"
+      node("master"){
+        withCredentials([
+          file(
+            credentialsId: 'id_rsa_cloud10_jenkins_file',
+            variable: 'JENKINS_SSH_PRIVKEY'
+          )]){
+            sh """#!/bin/bash -x
+              jdir=~jenkins
+              keyfile=\$jdir/id_webhookproxy
+              upstart_conf=\$jdir/webhookproxy.conf
+              cp \$JENKINS_SSH_PRIVKEY \$keyfile ||:
+              chmod 0400 \$keyfile
+              cat > \$upstart_conf <<EOF
+start on runlevel 2
+# ssh will not daemonize so use the default expect.
+# expect
+exec ssh root@$ip -i \$keyfile -R 8888:localhost:443 -N
+respawn
+EOF
+
+              cat > \$jdir/root_setup.sh <<EOF
+#!/bin/bash -x
+mkdir ~/.ssh -p
+ssh-keyscan $ip >> ~/.ssh/known_hosts
+cp \$upstart_conf /etc/init
+sleep 3;
+initctl reload webhookproxy ||:
+initctl stop webhookproxy
+initctl start webhookproxy
+EOF
+             chmod +x \$jdir/root_setup.sh
+
+              echo "Please run ~jenkins/root_setup.sh as root on the jenkins master"
+              """
+        } //withCredentials
+      } //node
+    }) //conditionalStage
+  pubCloudSlave.delPubCloudSlave()
+} //func
+return this
