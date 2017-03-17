0de3fedc478ec015da1807117a2ff93bb515c802
Hugh Saunders <hugh@wherenow.org>
2017-03-17 11:52:46 +0000

    Use ssh_retry in ansible 1.9
    Upstream ansible doesn't have functionality for retrying failed ssh
    connections before v2.
    
    As we often see ssh connection failures, use a fork of ansible 1.9
    that contains the ssh_retry connection plugin.
    
    This only affects mitaka and below as newton and above use ansible v2.
    
    Connects rcbops/u-suk-dev#1406

--- pipeline-steps/common.groovy
+++ pipeline-steps/common.groovy
@@ -97,7 +97,10 @@ def openstack_ansible(Map args){
         'ANSIBLE_FORCE_COLOR=true',
         'ANSIBLE_HOST_KEY_CHECKING=False',
         "ANSIBLE_FORKS=${forks}",
-        'ANSIBLE_SSH_RETRIES=3'])
+        'ANSIBLE_SSH_RETRIES=3',
+        'ANSIBLE_GIT_RELEASE=ssh_retry', //only used in mitaka and below
+        'ANSIBLE_GIT_REPO="https://github.com/hughsaunders/ansible"' // only used in mitaka and below
+        ])
       {
         sh """#!/bin/bash
           openstack-ansible ${args.playbook} ${args.args}
