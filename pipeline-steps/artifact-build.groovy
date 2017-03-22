14ea2dd1a86b0704e366346d230774d93e1659d4
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2017-03-22 11:51:19 +0000

    Add python/container artifact build jobs

---
+++ pipeline-steps/artifact-build.groovy
@@ -0,0 +1,35 @@
+def python(Map args) {
+  common.conditionalStage(
+    stage_name: "Build Python Artifacts",
+    stage: {
+      environment_vars = args.environment_vars
+      withEnv(environment_vars) {
+        ansiColor('xterm') {
+          dir("/opt/rpc-openstack/") {
+            sh """#!/bin/bash
+            scripts/artifacts-building/python/build-python-artifacts.sh
+            """
+          } // dir
+        } // ansiColor
+      } // withEnv
+    } // stage
+  ) // conditionalStage
+}
+
+def container(Map args) {
+  common.conditionalStage(
+    stage_name: "Build Container Artifacts",
+    stage: {
+      environment_vars = args.environment_vars
+      withEnv(environment_vars) {
+        ansiColor('xterm') {
+          dir("/opt/rpc-openstack/") {
+            sh """#!/bin/bash
+            scripts/artifacts-building/containers/build-process.sh
+            """
+          } // dir
+        } // ansiColor
+      } // withEnv
+    } // stage
+  ) // conditionalStage
+}
