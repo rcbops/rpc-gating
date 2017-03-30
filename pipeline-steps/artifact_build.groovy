283b545753c27a7567ebc35341316c3758e511bf
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2017-03-30 16:27:18 +0100

    Artifact job refactor
    This patch aims to refactor the Artifact jobs so
    that they more effectively use stages, and share
    a common structure. The detailed steps are all in
    the groovy file, whereas the pipeline logic is
    formed into stages in the job dsl.
    
    An additional job is also added to build apt
    artifacts. This job runs on a jenkins slave rather
    than a single-use node.
    
    A common script to get the credentials into the
    environment is used for all jobs.

--- pipeline-steps/artifact_build.groovy
+++ pipeline-steps/artifact_build.groovy
@@ -1,71 +1,100 @@
-def python(Map args) {
+def get_rpc_repo_creds(){
+  return [
+    string(
+      credentialsId: "RPC_REPO_IP",
+      variable: "REPO_HOST"
+    ),
+    string(
+      credentialsId: "RPC_REPO_SSH_USERNAME_TEXT",
+      variable: "REPO_USER"
+    ),
+    file(
+      credentialsId: "RPC_REPO_SSH_USER_PRIVATE_KEY_FILE",
+      variable: "REPO_USER_KEY"
+    ),
+    file(
+      credentialsId: "RPC_REPO_SSH_HOST_PUBLIC_KEY_FILE",
+      variable: "REPO_HOST_PUBKEY"
+    ),
+    file(
+      credentialsId: "RPC_REPO_GPG_SECRET_KEY_FILE",
+      variable: "GPG_PRIVATE"
+    ),
+    file(
+      credentialsId: "RPC_REPO_GPG_PUBLIC_KEY_FILE",
+      variable: "GPG_PUBLIC"
+    )
+  ]
+}
+
+def apt() {
   common.conditionalStage(
-    stage_name: "Build Python Artifacts",
+    stage_name: "Build Apt Artifacts",
     stage: {
-      withEnv(args.environment_vars) {
-        withCredentials([
-          string(
-            credentialsId: "RPC_REPO_IP",
-            variable: "REPO_HOST"
-          ),
-          string(
-            credentialsId: "RPC_REPO_SSH_USERNAME_TEXT",
-            variable: "REPO_USER"
-          ),
-          file(
-            credentialsId: "RPC_REPO_SSH_USER_PRIVATE_KEY_FILE",
-            variable: "REPO_USER_KEY"
-          ),
-          file(
-            credentialsId: "RPC_REPO_SSH_HOST_PUBLIC_KEY_FILE",
-            variable: "REPO_HOST_PUBKEY"
-          )
-        ]){
-          ansiColor('xterm') {
-            dir("/opt/rpc-openstack/") {
-              sh """#!/bin/bash
-              scripts/artifacts-building/python/build-python-artifacts.sh
-              """
-            } // dir
-          } // ansiColor
-        } // withCredentials
-      } // withEnv
+      withCredentials(get_rpc_repo_creds()) {
+        prepareRpcGit()
+        ansiColor('xterm') {
+          dir("/opt/rpc-openstack/") {
+            sh """#!/bin/bash
+            scripts/artifacts-building/apt/build-apt-artifacts.sh
+            """
+          } // dir
+        } // ansiColor
+      } // withCredentials
     } // stage
   ) // conditionalStage
 }
 
-def container(Map args) {
+def python() {
+  common.conditionalStage(
+    stage_name: "Build Python Artifacts",
+    stage: {
+      pubcloud.runonpubcloud {
+        try {
+          withCredentials(get_rpc_repo_creds()) {
+            prepareRpcGit()
+            ansiColor('xterm') {
+              dir("/opt/rpc-openstack/") {
+                sh """#!/bin/bash
+                scripts/artifacts-building/python/build-python-artifacts.sh
+                """
+              } // dir
+            } // ansiColor
+          } // withCredentials
+        } catch (e) {
+          print(e)
+          throw e
+        } finally {
+          common.archive_artifacts()
+        }
+      } // pubcloud slave
+    } // stage
+  ) // conditionalStage
+}
+
+def container() {
   common.conditionalStage(
     stage_name: "Build Container Artifacts",
     stage: {
-      withEnv(args.environment_vars) {
-        withCredentials([
-          string(
-            credentialsId: "RPC_REPO_IP",
-            variable: "REPO_HOST"
-          ),
-          string(
-            credentialsId: "RPC_REPO_SSH_USERNAME_TEXT",
-            variable: "REPO_USER"
-          ),
-          file(
-            credentialsId: "RPC_REPO_SSH_USER_PRIVATE_KEY_FILE",
-            variable: "REPO_USER_KEY"
-          ),
-          file(
-            credentialsId: "RPC_REPO_SSH_HOST_PUBLIC_KEY_FILE",
-            variable: "REPO_HOST_PUBKEY"
-          )
-        ]){
-          ansiColor('xterm') {
-            dir("/opt/rpc-openstack/") {
-              sh """#!/bin/bash
-              scripts/artifacts-building/containers/build-process.sh
-              """
-            } // dir
-          } // ansiColor
-        } // withCredentials
-      } // withEnv
+      pubcloud.runonpubcloud {
+        try {
+          withCredentials(get_rpc_repo_creds()) {
+            prepareRpcGit()
+            ansiColor('xterm') {
+              dir("/opt/rpc-openstack/") {
+                sh """#!/bin/bash
+                scripts/artifacts-building/containers/build-process.sh
+                """
+              } // dir
+            } // ansiColor
+          } // withCredentials
+        } catch (e) {
+          print(e)
+          throw e
+        } finally {
+          common.archive_artifacts()
+        }
+      } // pubcloud slave
     } // stage
   ) // conditionalStage
 }
