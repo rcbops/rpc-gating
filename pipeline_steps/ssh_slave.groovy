87e82a43a93084a9242bd5151acef77121e2f377
Matt Thompson <mattt@defunct.ca>
2017-04-10 13:49:03 -0400

    Create separate slave step/stage
    This commit moves the step / stage for connecting and destroying a
    jenkins slave from pubcloud.groovy to ssh_slave.groovy so that these
    steps can be properly included in a non pubcloud context.

--- pipeline_steps/ssh_slave.groovy
+++ pipeline_steps/ssh_slave.groovy
@@ -6,52 +6,61 @@
  *  - playbooks/inventory/hosts
  */
 def connect(){
-  withCredentials([
-    file(
-      credentialsId: 'id_rsa_cloud10_jenkins_file',
-      variable: 'JENKINS_SSH_PRIVKEY'
-    ),
-    usernamePassword(
-      credentialsId: "service_account_jenkins_api_creds",
-      usernameVariable: "JENKINS_USERNAME",
-      passwordVariable: "JENKINS_API_KEY"
-    )
-  ]){
-    dir("rpc-gating/playbooks"){
-      common.venvPlaybook(
-        playbooks: ["setup_jenkins_slave.yml"],
-        venv: ".venv",
-        args: [
-          "-i inventory",
-          "--limit job_nodes",
-          "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\""
-        ]
-      )
-    } //dir
-  } //withCredentials
+  common.conditionalStage(
+    stage_name: "Connect Slave",
+    stage: {
+      withCredentials([
+        file(
+          credentialsId: 'id_rsa_cloud10_jenkins_file',
+          variable: 'JENKINS_SSH_PRIVKEY'
+        ),
+        usernamePassword(
+          credentialsId: "service_account_jenkins_api_creds",
+          usernameVariable: "JENKINS_USERNAME",
+          passwordVariable: "JENKINS_API_KEY"
+        )
+      ]){
+        dir("rpc-gating/playbooks"){
+          common.venvPlaybook(
+            playbooks: ["setup_jenkins_slave.yml"],
+            venv: ".venv",
+            args: [
+              "-i inventory",
+              "--limit job_nodes",
+              "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\""
+            ]
+          )
+        } //dir
+      } //withCredentials
+  }) //conditionalStage
 } //call
 
 /* Disconnect slave
  * Reads global var: instance_name
  */
 def destroy(){
-  withCredentials([
-    usernamePassword(
-      credentialsId: "service_account_jenkins_api_creds",
-      usernameVariable: "JENKINS_USERNAME",
-      passwordVariable: "JENKINS_API_KEY"
-    )
-  ]){
-    dir("rpc-gating/scripts"){
-      sh """
-        . ../playbooks/.venv/bin/activate
-        pip install 'pip==9.0.1'
-        pip install -c ../constraints.txt jenkinsapi
-        python jenkins_node.py \
-          delete --name "${instance_name}"
-      """
-    } //dir
-  } //withCredentials
+  common.conditionalStep(
+    step_name: 'Destroy Slave',
+    step: {
+      withCredentials([
+        usernamePassword(
+          credentialsId: "service_account_jenkins_api_creds",
+          usernameVariable: "JENKINS_USERNAME",
+          passwordVariable: "JENKINS_API_KEY"
+        )
+      ]){
+        dir("rpc-gating/scripts"){
+          sh """
+            . ../playbooks/.venv/bin/activate
+            pip install 'pip==9.0.1'
+            pip install -c ../constraints.txt jenkinsapi
+            python jenkins_node.py \
+              delete --name "${instance_name}"
+          """
+        } //dir
+      } //withCredentials
+    } //stage
+  ) //conditionalStage
 } //call
 
 return this
