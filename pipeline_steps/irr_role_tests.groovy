7c2aebbadf82b1b516d3624e83e72f5a6a647344
Kevin Carter <kevin.carter@rackspace.com>
2017-04-27 12:16:28 -0500

    Added job for generic irr testing
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

---
+++ pipeline_steps/irr_role_tests.groovy
@@ -0,0 +1,72 @@
+def run_irr_tests() {
+  common.conditionalStage(
+    stage_name: "Run IRR Tests",
+    stage: {
+      pubcloud.runonpubcloud {
+        try {
+          ansiColor('xterm') {
+            dir("${env.WORKSPACE}/${env.ghprbGhRepository}") {
+              print("Triggered by PR: ${env.ghprbPullLink}")
+              checkout([$class: 'GitSCM',
+                branches: [[name: "origin/pr/${env.ghprbPullId}/merge"]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[$class: 'CleanCheckout']],
+                submoduleCfg: [],
+                userRemoteConfigs: [
+                  [
+                    url: "https://github.com/${env.ghprbGhRepository}.git",
+                    refspec: '+refs/pull/*:refs/remotes/origin/pr/* +refs/heads/*:refs/remotes/origin/*'
+                  ]
+                ]
+              ]) // checkout
+              sh """#!/bin/bash
+              bash ./run_tests.sh
+              """
+            } // dir
+          } // ansiColor
+        } catch (e) {
+          print(e)
+          throw e
+        } finally {
+          irr_archive_artifacts()
+        }
+      } // pubcloud slave
+    } // stage
+  ) // conditionalStage
+}
+
+def irr_archive_artifacts(){
+  print("Running Log Collection")
+  try{
+    sh """#!/bin/bash
+    d="artifacts_\${BUILD_TAG}"
+    mkdir -p "${WORKSPACE}/logs"
+    pushd "${WORKSPACE}/logs"
+      touch "\$d".marker
+    popd
+    tar -C "${WORKSPACE}/logs" -cjf "${env.WORKSPACE}/\$d".tar.bz2 .
+    """
+  } catch (e){
+    print(e)
+    throw(e)
+  } finally{
+    // still worth trying to archiveArtifacts even if some part of
+    // artifact collection failed.
+      pubcloud.uploadToCloudFiles(
+        container: "jenkins_logs",
+        src: "${env.WORKSPACE}/artifacts_${env.BUILD_TAG}.tar.bz2",
+        html_report_dest: "${env.WORKSPACE}/artifacts_report/index.html")
+      publishHTML(
+        allowMissing: true,
+        alwaysLinkToLastBuild: true,
+        keepAll: true,
+        reportDir: 'artifacts_report',
+        reportFiles: 'index.html',
+        reportName: 'Build Artifact Links'
+      )
+    sh """
+    rm -rf "${WORKSPACE}/logs"
+    rm -f artifacts_${env.BUILD_TAG}.tar.bz2
+    """
+  }
+}
