08cbeac23776951690bdfa8d4c67a0e7bb7f58bf
Matt Thompson <mattt@defunct.ca>
2017-04-27 12:37:08 -0400

    Job parity and phantomjs update
    This commit does the following to rpc_aio.yml:
    
    1. Adds `Prepare Kibana Selenium`, 'Kibana Tests', and `Holland` to
       STAGES and breaks up the excessively long string
    2. Executes kibana.kibana() in the job template DSL
    
    It also does the following to lem_multi_node_aio.yml:
    
    1. Updates concurrent so we can multiple jobs simultaneously
    
    Lastly, we update kibana.groovy:
    
    1. Install phantomjs from binary, rather than Ubuntu package (this
       package is problematic on Ubuntu 16.04)
    2. Adjusts some shell indentation for easier reading
    
    Connects https://github.com/rcbops/u-suk-dev/issues/1491
    Connects https://github.com/rcbops/u-suk-dev/issues/1614

--- pipeline_steps/kibana.groovy
+++ pipeline_steps/kibana.groovy
@@ -18,20 +18,26 @@ def kibana_prep(){
     git url: "https://github.com/rcbops-qe/kibana-selenium.git", branch: "master"
 
     sh """#!/bin/bash
-    apt-get install -y phantomjs
+      # The phantomjs package on 16.04 is buggy, see:
+      # https://github.com/ariya/phantomjs/issues/14900
+      # https://bugs.launchpad.net/ubuntu/+source/phantomjs/+bug/1578444
+      #apt-get install -y phantomjs
+      apt-get install -y fontconfig
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+      tar -xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 
-    if [[ ! -d ".venv" ]]; then
-        pip install virtualenv
-        virtualenv .venv
-    fi
-    source .venv/bin/activate
-    if [ -f ~/.pip/pip.conf ]; then
-      mv ~/.pip/pip.conf ~/.pip/pip.conf.bak
-      pip install -r requirements.txt
-      mv ~/.pip/pip.conf.bak ~/.pip/pip.conf
-    else
-      pip install -r requirements.txt
-    fi
+      if [[ ! -d ".venv" ]]; then
+          pip install virtualenv
+          virtualenv .venv
+      fi
+      source .venv/bin/activate
+      if [ -f ~/.pip/pip.conf ]; then
+        mv ~/.pip/pip.conf ~/.pip/pip.conf.bak
+        pip install -r requirements.txt
+        mv ~/.pip/pip.conf.bak ~/.pip/pip.conf
+      else
+        pip install -r requirements.txt
+      fi
     """
   }
 }
@@ -41,13 +47,14 @@ def kibana_tests(){
     dir("kibana-selenium") {
       git url: "https://github.com/rcbops-qe/kibana-selenium.git", branch: "master"
       sh """#!/bin/bash
-      source .venv/bin/activate
-      export PYTHONPATH=\$(pwd)
-      # Remove any existing screenshots from old runs
-      rm -f *.png
-      python conf-gen.py --secure --password-file /etc/openstack_deploy/user_rpco_secrets.yml \
-        --vip-file /etc/openstack_deploy/openstack_user_config.yml
-      nosetests -sv --with-xunit testrepo/kibana/kibana.py
+        source .venv/bin/activate
+        export PYTHONPATH=\$(pwd)
+        export PATH=\$PATH:./phantomjs-2.1.1-linux-x86_64/bin
+        # Remove any existing screenshots from old runs
+        rm -f *.png
+        python conf-gen.py --secure --password-file /etc/openstack_deploy/user_rpco_secrets.yml \
+          --vip-file /etc/openstack_deploy/openstack_user_config.yml
+        nosetests -sv --with-xunit testrepo/kibana/kibana.py
       """
     } // dir
   } catch (e){
