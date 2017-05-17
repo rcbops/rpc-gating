65860a20c89277af7c2657afa336c9dd493e8783
Melissa Kam <melissa.kam@rackspace.com>
2017-05-17 16:06:11 -0500

    UG-520: Add OSA SHA bump PR proposal job

--- scripts/ghutils.py
+++ scripts/ghutils.py
@@ -37,12 +37,46 @@ def create_issue(tag, link, org, repo, pat, label):
     )
 
 
+@click.command()
+@click.option('--org',
+              help='Github Organisation that owns the target repo',
+              required=True)
+@click.option('--repo',
+              help='Name of target repo',
+              required=True)
+@click.option('--pat',
+              help="Github Personal Access Token",
+              required=True)
+@click.option('--title',
+              help="Title of pull request",
+              required=True)
+@click.option('--base',
+              help="Base branch to PR onto",
+              required=True)
+@click.option('--head',
+              help="Branch to create a pull request for",
+              required=True)
+@click.option('--body',
+              help="Body of pull request",
+              required=True)
+def create_pr(org, repo, pat, title, base, head, body):
+    gh = github3.login(token=pat)
+    repo = gh.repository(org, repo)
+    repo.create_pull(
+        title=title,
+        base=base,
+        head=head,
+        body=body
+    )
+
+
 @click.group()
 def cli():
     pass
 
 
 cli.add_command(create_issue)
+cli.add_command(create_pr)
 
 if __name__ == "__main__":
     cli()
