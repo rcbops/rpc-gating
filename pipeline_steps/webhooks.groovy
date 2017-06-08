ccc6290ba2ba7193ebdb3d309fd5aab06265e3eb
Hugh Saunders <hugh@wherenow.org>
2017-06-08 14:13:32 +0100

    UG-618 Add exception handler to webhooks.groovy
    This will allow test jobs to cleanup after themselves even if an
    exception is thrown. Note that by default the cleanup stages are not run
    as this job is intended to create a long running node.
    
    This is a separate commit so that changes to isolate the addition
    of the try block from other changes to the file.

--- pipeline_steps/webhooks.groovy
+++ pipeline_steps/webhooks.groovy
@@ -1,64 +1,65 @@
 def webhooks(){
   instance_name = "WEBHOOK-PROXY"
   pubCloudSlave.getPubCloudSlave(instance_name: instance_name)
-  common.conditionalStage(
-    stage_name: "Webhooks",
-    stage:{
-      withCredentials([
-        file(
-          credentialsId: 'id_rsa_cloud10_jenkins_file',
-          variable: 'jenkins_ssh_privkey'
-        ),
-        usernamePassword(
-          credentialsId: "github_webhook_proxy_basic_auth",
-          usernameVariable: "webhookproxy_user",
-          passwordVariable: "webhookproxy_pass"
-        ),
-        usernamePassword(
-          credentialsId: "jira_user_pass",
-          usernameVariable: "JIRA_USER",
-          passwordVariable: "JIRA_PASS"
-        ),
-        string(
-          credentialsId: "RACKSPACE_WEBHOOK_TOKEN",
-          variable: "RACKSPACE_WEBHOOK_TOKEN"
-        ),
-        string(
-          credentialsId: "SSH_IP_ADDRESS_WHITELIST",
-          variable: "SSH_IP_ADDRESS_WHITELIST"
-        ),
-      ]){
-        dir('rpc-gating/playbooks'){
-          common.venvPlaybook(
-            playbooks: [
-              "webhooks.yml",
-            ],
-            args: [
-              "-i inventory",
-              "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\""
-            ],
-            vars: [
-              WORKSPACE: "${env.WORKSPACE}",
-              webhookproxy_user: "${env.webhookproxy_user}",
-              webhookproxy_pass: "${env.webhookproxy_pass}"
-            ]
-          ) //venvPlaybook
-        } //dir
-      } //withCredentials
-      ip = readFile file: "instance_address"
-      node("master"){
+  try{
+    common.conditionalStage(
+      stage_name: "Webhooks",
+      stage:{
         withCredentials([
           file(
             credentialsId: 'id_rsa_cloud10_jenkins_file',
-            variable: 'JENKINS_SSH_PRIVKEY'
-          )]){
-            sh """#!/bin/bash -x
-              jdir=~jenkins
-              keyfile=\$jdir/id_webhookproxy
-              upstart_conf=\$jdir/webhookproxy.conf
-              cp \$JENKINS_SSH_PRIVKEY \$keyfile ||:
-              chmod 0400 \$keyfile
-              cat > \$upstart_conf <<EOF
+            variable: 'jenkins_ssh_privkey'
+          ),
+          usernamePassword(
+            credentialsId: "github_webhook_proxy_basic_auth",
+            usernameVariable: "webhookproxy_user",
+            passwordVariable: "webhookproxy_pass"
+          ),
+          usernamePassword(
+            credentialsId: "jira_user_pass",
+            usernameVariable: "JIRA_USER",
+            passwordVariable: "JIRA_PASS"
+          ),
+          string(
+            credentialsId: "RACKSPACE_WEBHOOK_TOKEN",
+            variable: "RACKSPACE_WEBHOOK_TOKEN"
+          ),
+          string(
+            credentialsId: "SSH_IP_ADDRESS_WHITELIST",
+            variable: "SSH_IP_ADDRESS_WHITELIST"
+          ),
+        ]){
+          dir('rpc-gating/playbooks'){
+            common.venvPlaybook(
+              playbooks: [
+                "webhooks.yml",
+              ],
+              args: [
+                "-i inventory",
+                "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\""
+              ],
+              vars: [
+                WORKSPACE: "${env.WORKSPACE}",
+                webhookproxy_user: "${env.webhookproxy_user}",
+                webhookproxy_pass: "${env.webhookproxy_pass}"
+              ]
+            ) //venvPlaybook
+          } //dir
+        } //withCredentials
+        ip = readFile file: "instance_address"
+        node("master"){
+          withCredentials([
+            file(
+              credentialsId: 'id_rsa_cloud10_jenkins_file',
+              variable: 'JENKINS_SSH_PRIVKEY'
+            )]){
+              sh """#!/bin/bash -x
+                jdir=~jenkins
+                keyfile=\$jdir/id_webhookproxy
+                upstart_conf=\$jdir/webhookproxy.conf
+                cp \$JENKINS_SSH_PRIVKEY \$keyfile ||:
+                chmod 0400 \$keyfile
+                cat > \$upstart_conf <<EOF
 start on runlevel 2
 # ssh will not daemonize so use the default expect.
 # expect
@@ -66,7 +67,7 @@ exec ssh root@$ip -i \$keyfile -R 8888:localhost:443 -N
 respawn
 EOF
 
-              cat > \$jdir/root_setup.sh <<EOF
+                cat > \$jdir/root_setup.sh <<EOF
 #!/bin/bash -x
 mkdir ~/.ssh -p
 ssh-keyscan $ip >> ~/.ssh/known_hosts
@@ -76,13 +77,18 @@ initctl reload webhookproxy ||:
 initctl stop webhookproxy
 initctl start webhookproxy
 EOF
-             chmod +x \$jdir/root_setup.sh
+               chmod +x \$jdir/root_setup.sh
 
-              echo "Please run ~jenkins/root_setup.sh as root on the jenkins master"
-              """
-        } //withCredentials
-      } //node
-    }) //conditionalStage
-  pubCloudSlave.delPubCloudSlave()
+                echo "Please run ~jenkins/root_setup.sh as root on the jenkins master"
+                """
+            } //withCredentials
+          } //node
+        }) //conditionalStage
+  } catch (e){
+    print(e)
+    throw e
+  }finally{
+    pubCloudSlave.delPubCloudSlave()
+  }
 } //func
 return this
