c3b6187419717a142a09aa878d2a9badfe90498b
Hugh Saunders <hugh@wherenow.org>
2017-06-22 15:53:42 +0100

    fix periodic cleanup on Ubuntu slave

--- scripts/tmp_cleanup.sh
+++ scripts/tmp_cleanup.sh
@@ -2,6 +2,7 @@
 
 echo "Remove old pip build directories"
 set -xe
+mkdir -p /var/lib/jenkins/tmp
 cd /var/lib/jenkins/tmp \
   && find . -maxdepth 1 -mtime +2 -type d -name "pip*" \
     -exec rm -rf {} \;
