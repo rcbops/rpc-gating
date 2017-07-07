a4054d518c5281473bb2d9c6988500f55dcb0ca8
git-harry <git-harry@live.co.uk>
2017-07-07 14:06:21 +0100

    Allow configuring the number of MNAIO nodes
    This change adds support for configuring the number of nova compute and
    cinder volume nodes on a multi-node AIO.
    
    The following build parameters can be set to configure the number of
    nodes:
      - COMPUTE_NODES
      - VOLUME_NODES

--- pipeline_steps/maas.groovy
+++ pipeline_steps/maas.groovy
@@ -1,7 +1,35 @@
+String get_mnaio_hostnames() {
+  dir("openstack-ansible-ops/${env.MULTI_NODE_AIO_DIR}") {
+    entities = sh (
+      script: """#!/usr/bin/env python
+from __future__ import print_function
+from itertools import chain
+import json
+
+with open("hosts.json") as f:
+    data = f.read()
+
+hostnames = ["loadbalancer1"]
+hostnames.extend(
+    chain(*(node_type.keys() for node_type in json.loads(data).values()))
+)
+print(
+    json.dumps(
+        ["%s.{{ server_name }}" % hostname for hostname in hostnames]
+    )
+)
+      """,
+      returnStdout: true,
+    )
+  }
+  return entities
+}
+
 def prepare(Map args) {
   common.conditionalStage(
     stage_name: 'Prepare MaaS',
     stage: {
+      entities = get_mnaio_hostnames()
       withCredentials(common.get_cloud_creds()){
         dir("rpc-gating/playbooks"){
           pyrax_cfg = common.writePyraxCfg(
@@ -13,7 +41,8 @@ def prepare(Map args) {
             common.venvPlaybook(
               playbooks: ['multi_node_aio_maas_entities.yml'],
               args: [
-                "-e server_name=\"${args.instance_name}\""
+                "-e server_name=\"${args.instance_name}\"",
+                "-e '{\"entity_labels\": ${entities}}'",
               ],
               vars: args
             )
@@ -69,6 +98,7 @@ def entity_cleanup(Map args){
   common.conditionalStep(
     step_name: 'Cleanup',
     step: {
+      entities = get_mnaio_hostnames()
       withCredentials(common.get_cloud_creds()) {
         dir("rpc-gating/playbooks") {
           common.install_ansible()
@@ -81,6 +111,7 @@ def entity_cleanup(Map args){
               playbooks: ['multi_node_aio_maas_cleanup.yml'],
               vars: [
                 "server_name": args.instance_name,
+                "entity_labels": entities,
               ]
             )
           } // withEnv
