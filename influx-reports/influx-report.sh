47769017663aa83a20289d83f1e800fe82d0c3f3
git-harry <git-harry@live.co.uk>
2017-08-15 22:44:25 +0100

    LA-412 Update influxdb reporting script
    This change adds use of `influx-report.sh` to the leapfrog upgrade jobs.
    This results in the console log including a breakdown of the downtime
    seen by the MaaS plugins during the leapfrog upgrade process.
    
    The Python script `influx.py` has been reworked to allow for a more
    detailed report to be produced for leapfrog upgrades. This makes use of
    the `.leap` and `.complete` marker files, created at different stages of
    the upgrade, to associate the data stored in InfluxDB with the relevant
    part of the upgrade. The marker files do not perfectly align with the
    upgrades steps but are of sufficient accuracy for use in providing the
    breakdown.
    
    If the script is used on a deployment that is not a leapfrog upgrade, it
    will still produce overall statistics for the build based solely on the
    data in InfluxDB.

--- influx-reports/influx-report.sh
+++ influx-reports/influx-report.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 PYTHON_PATH="${WORKSPACE}/.venv/bin/python"
 REPORTS_FOLDER="${WORKSPACE}/test_results"
 INFLUX_DOWNTIME_FOLDER="$(dirname "${BASH_SOURCE[0]}")"
+ADDITIONAL_ARGS="$*"
 
+pushd "$WORKSPACE"
+  virtualenv .venv
+  . .venv/bin/activate
+  pip install -r rpc-gating/requirements.txt -c rpc-gating/constraints.txt
+popd
 mkdir -p "${REPORTS_FOLDER}"
-$PYTHON_PATH $INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-uptime.yml --subunitreport $REPORTS_FOLDER/influx-uptime.bin
+$INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-downtime.yml --subunitreport $REPORTS_FOLDER/influx-downtime.bin $ADDITIONAL_ARGS
--- influx-reports/influx.py
+++ influx-reports/influx.py
@@ -1,31 +1,71 @@
-#!/usr/bin/env python3
-""" Get all measurements for an influx job, and
-outputs a report of uptime for maas_ components.
-Inputs:
-    - env.INFLUX_IP (mandatory)
-    - env.INFLUX_PORT (mandatory)
-    - env.BUILD_TAG (mandatory)
-    - ymlreport <ymlreportfile> (optional)
-    - subunitreport <subunitreport> (optional)
-Outputs:
-    - stdout
-    - yaml file with uptime (when ymlreport is used)
-    - subunit binary file with passing/failure criteria
-    (when subunitreport is used)
-"""
+#!/usr/bin/env python
 
 import argparse
 from collections import defaultdict
-import datetime
+from datetime import datetime, timedelta
+from copy import deepcopy
 import logging
 import os
-import math
 import sys
+
+import dateutil
+from influxdb import InfluxDBClient
+import pytz
+from subunit.v2 import StreamResultToBytes
 import yaml
 
-import dateutil.parser
-from influxdb import InfluxDBClient
-from subunit.v2 import StreamResultToBytes
+
+LEAPFROG_STAGES = [
+    {
+        "stage_name": "RPCO/ubuntu14-leapfrog.sh",
+        "filename": "deploy-rpc.complete",
+        "stages": [
+            {
+                "stage_name": "RPCO/pre_leap.sh",
+                "filename": "rpc-prep.complete",
+            },
+            {
+                "stage_name": "OSA-OPS/run-stages.sh",
+                "filename": "osa-leap.complete",
+                "stages": [
+                    {
+                        "stage_name": "OSA-OPS/prep.sh",
+                        "filename": "openstack-ansible-prep-finalsteps.leap",
+                    },
+                    {
+                        "stage_name": "OSA-OPS/upgrade.sh",
+                        "filename": (
+                            "openstack-ansible-upgrade-hostupgrade.leap"
+                        ),
+                    },
+                    {
+                        "stage_name": "OSA-OPS/migrations.sh",
+                        "filename": "openstack-ansible-14.2.3-db.leap",
+                    },
+                    {
+                        "stage_name": "OSA-OPS/redeploy.sh",
+                        "filename": "osa-leap.complete",
+                        "stages": [
+                            {
+                                "stage_name": "RPCO/pre_redeploy.sh",
+                                "filename": (
+                                    "rebootstrap-ansible-for-rpc.complete"
+                                ),
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "stage_name": "RPCO/post_leap.sh",
+                "filename": "deploy-rpc.complete",
+            },
+        ]
+    },
+]
+
+
+yaml.Dumper.ignore_aliases = lambda *args: True
 
 
 class InfluxTimestampParseException(Exception):
@@ -39,7 +79,7 @@ class SubunitContext():
         self.output_path = output_path
 
     def __enter__(self):
-        self.output_stream = open(self.output_path, 'wb+')
+        self.output_stream = open(self.output_path, "wb+")
         self.output = StreamResultToBytes(self.output_stream)
         self.output.startTestRun()
         return self.output
@@ -48,8 +88,113 @@ class SubunitContext():
         self.output.stopTestRun()
         self.output_stream.close()
 
-    def status(self, *args, **kwargs):
-        self.output.status(*args, **kwargs)
+
+def generate_reports(data, max_downtime=100, ymlfile=None, subunitfile=None):
+    yml_report = yaml.dump(data, default_flow_style=False)
+    logging.info(yml_report)
+    if ymlfile:
+        with open(ymlfile, "w+") as output_file:
+            output_file.write("---\n")
+            output_file.write(yml_report)
+
+    if subunitfile:
+        with SubunitContext(subunitfile) as output:
+            for stage in data:
+                for svc_name, svc_data in stage["services_down"].items():
+                    status = "fail"
+                    if svc_data["percentage"] < max_downtime:
+                        status = "success"
+                    # Record test start
+                    output.status(
+                        test_id=svc_name,
+                        timestamp=stage["started"]
+                    )
+
+                    # Record end of test
+                    output.status(
+                        test_id=svc_name,
+                        # TODO(hughsaunders): Be more intelligent
+                        # about thresholds
+                        test_status=status,
+                        test_tags=None,
+                        runnable=False,
+                        file_name=svc_name,
+                        file_bytes=" {}% higher than threshold".format(
+                            svc_data["percentage"]).encode("ascii"),
+                        timestamp=stage["completed"],
+                        eof=True,
+                        mime_type="text/plain; charset=UTF8"
+                    )
+
+
+def get_mtime(filename):
+    leapfiledir = "/opt/rpc-leapfrog/leap42"
+    completefiledir = "/etc/openstack_deploy/upgrade-leap"
+    if filename.endswith(".complete"):
+        directory = completefiledir
+    elif filename.endswith(".leap"):
+        directory = leapfiledir
+    else:
+        directory = "./"
+    path = os.path.join(directory, filename)
+    file_stats = os.stat(path)
+
+    return datetime.utcfromtimestamp(file_stats.st_mtime).replace(
+        microsecond=0, tzinfo=pytz.utc,
+    )
+
+
+def get_downtime(client, build, start, end):
+    measurements = [
+        "maas_glance", "maas_cinder", "maas_keystone",
+        "maas_heat", "maas_neutron", "maas_nova", "maas_horizon"
+    ]
+    resolution = 60
+
+    start_rounded = start.replace(second=0)
+    end_rounded = end.replace(second=0) + timedelta(minutes=1)
+    max_downtime_rounded = (end_rounded - start_rounded).total_seconds()
+
+    service_downtime = defaultdict(lambda: max_downtime_rounded)
+
+    for measurement in measurements:
+        query = (
+            "select max(/.*_status/) "
+            "from {measurement} "
+            "where time > '{start:%Y-%m-%d %H:%M:%S}' "
+            "and time < '{end:%Y-%m-%d %H:%M:%S}' "
+            "and job_reference='{build}' "
+            "group by time({resolution}s) fill(-1)"
+        ).format(
+            start=start,
+            end=end,
+            build=build,
+            measurement=measurement,
+            resolution=resolution,
+        )
+
+        all_data = client.query(query)
+        elements = (
+            (name, value)
+            for time_slice in all_data.get_points()
+            for name, value in time_slice.items()
+            if name != "time"
+        )
+        for name, value in elements:
+            key = str(name.replace("max_", "").replace("_status", ""))
+            if value == 1:
+                service_downtime[key] -= resolution
+            else:
+                # Ensure all services are added to the defaultdict
+                service_downtime[key]
+
+    return {
+        svc: {
+            "hh:mm:ss": str(timedelta(seconds=secs)),
+            "percentage": round(100 * secs / max_downtime_rounded, 1),
+        }
+        for svc, secs in service_downtime.items()
+    }
 
 
 def return_time(client, query, delta_seconds=0):
@@ -61,177 +206,115 @@ def return_time(client, query, delta_seconds=0):
     timestamp_query = client.query(query)
     # Get points is generator, we should just get first
     # point time (string type).
-    fpt_str = next(timestamp_query.get_points())['time']
+    fpt_str = next(timestamp_query.get_points())["time"]
     try:
-        fpt = dateutil.parser.parse(
-            fpt_str) + datetime.timedelta(seconds=delta_seconds)
-    except Exception as alle:
+        fpt = (
+            dateutil.parser.parse(fpt_str) +
+            timedelta(seconds=delta_seconds)
+        )
+    except Exception as e:
         raise InfluxTimestampParseException(
-            "Error parsing a timestamp from influx: {}".format(alle))
+            "Error parsing a timestamp from influx: {}".format(e)
+        )
     return fpt
 
 
-def calculate_measurement_uptime(client, measurement, job_reference,
-                                 measurement_period_seconds,
-                                 resolution=60):
-    """ For a certain build (job_reference), estimate the amount
-    of seconds a component (column of table) was up.
-    For that, we slice the job by resolution.
-    If I have no data in the slice, I assume downtime. If I have
-    at least one data per slice, I assume up, even if degraded.
-    The count of these positive events multiplied by resolution
-    is an average idea of the uptime.
-    """
-    per_field_up_percent = defaultdict(float)
+def add_time(client, build, stages, started):
+    for stage in stages:
+        completed = get_mtime(stage["filename"])
+        del stage["filename"]
+        duration = completed - started
+        stage["started"] = started
+        stage["completed"] = completed
+        stage["duration"] = str(duration)
+        stage["services_down"] = get_downtime(
+            client, build, started, completed
+        )
+        add_time(client, build, stage.get("stages", []), started)
+        started = completed
 
-    query = (
-        "select max(/.*_status/) from {measurement} "
-        "where time < now() and job_reference='{job_reference}' "
-        "group by time({resolution}s) fill(-1)"
-    ).format(measurement=measurement,
-             job_reference=job_reference,
-             resolution=resolution)
 
-    max_amount_of_time_slices = math.ceil(
-        float(measurement_period_seconds) / resolution)
+def get_build_data(client, build, leapfrog=False):
+    if leapfrog:
+        # This is the first marker file created and so approximately the start
+        start_time = get_mtime("clone.complete")
+        stages = deepcopy(LEAPFROG_STAGES)
+        add_time(client, build, stages, start_time)
+    else:
+        # First find the first and last timestamp from telegraf.
+        # This way querying maas_* data will always
+        # be accurate (if no data is reported by maas plugins,
+        # this should be a failure metric)
+        find_first_timestamp_query = (
+            "select first(total) from processes "
+            "where job_reference = '{}';".format(build)
+        )
+        started = return_time(
+            client, find_first_timestamp_query, delta_seconds=5
+        )
+        find_last_timestamp_query = (
+            "select last(total) from processes "
+            "where job_reference = '{}';".format(build))
+        completed = return_time(
+            client, find_last_timestamp_query, delta_seconds=-5
+        )
+        services_down = get_downtime(
+            client, build, started, completed
+        )
+        stages = [
+            {
+                "stage_name": "Upgrade",
+                "services_down": services_down,
+                "stages": [],
+                "started": started,
+                "completed": completed,
+                "duration": str(completed - started),
+            }
+        ]
 
-    all_data = client.query(query)
-    for time_slice in all_data.get_points():
-        for element in time_slice:
-            if element != 'time' and time_slice[element] == 1:
-                key = element.replace('max_', '').replace('_status', '')
-                # Mark the field as up
-                per_field_up_percent[key] += (1 / max_amount_of_time_slices)
-                # Mark the measurement as up
+    return stages
 
-    return dict(
-        per_field_uptime_percent=dict(per_field_up_percent)
+
+def main(args):
+    client = InfluxDBClient(
+        args.influx_ip, args.influx_port, database="telegraf"
     )
-
-
-def build_report_dict(client, measurements, job_reference,
-                      measurement_period_seconds):
-    report = defaultdict(dict)
-    for measurement in measurements:
-        measurement_data = calculate_measurement_uptime(
-            client, measurement, job_reference, measurement_period_seconds)
-        report['runtime_seconds'] = measurement_period_seconds
-        report['per_field_uptime_percent'][measurement] = \
-            dict((measure, round(measure_uptime * 100, 2)) for
-                 measure, measure_uptime in
-                 measurement_data['per_field_uptime_percent'].items())
-    return dict(report)
-
-
-def main(args=None):
-    client = InfluxDBClient(args.influx_ip, args.influx_port,
-                            database='telegraf')
-    """
-    Influx structure:
-        Measurement (eg maas_nova)
-            Point (a timestamp with data)
-                Tags (Indexed K/V, eg job_reference)
-                Fields (Unindexed K/V, eg keystone_user_count)
-    """
-    # First find the first and last timestamp from telegraf.
-    # This way querying maas_* data will always
-    # be accurate (if no data is reported by maas plugins,
-    # this should be a failure metric)
-    find_first_timestamp_query = (
-        "select first(total) from processes "
-        "where job_reference = '{}';".format(args.job_reference)
+    stages = get_build_data(
+        client, args.build_ref, leapfrog=args.leapfrog_upgrade
     )
-    first_ts = return_time(client, find_first_timestamp_query, delta_seconds=5)
-    find_last_timestamp_query = (
-        "select last(total) from processes "
-        "where job_reference = '{}';".format(args.job_reference))
-    last_ts = return_time(client, find_last_timestamp_query, delta_seconds=-5)
-
-    measurement_period_seconds = (last_ts - first_ts).total_seconds()
-
-    logging.info(
-        ("Metrics were gathered between {first} and {last}\n"
-         "For a total of {seconds} seconds".format(
-             first=first_ts,
-             last=last_ts,
-             seconds=measurement_period_seconds)))
-
-    measurements = ['maas_glance', 'maas_cinder', 'maas_keystone',
-                    'maas_heat', 'maas_neutron', 'maas_nova', 'maas_horizon']
-
-    report = build_report_dict(
-        client, measurements, args.job_reference, measurement_period_seconds)
-
-    yml_report = yaml.safe_dump(report, default_flow_style=False)
-    logging.info(yml_report)
-    if args.ymlreport:
-        with open(args.ymlreport, 'w+') as output_file:
-            output_file.write("---\n")
-            output_file.write(yml_report)
-
-    # Subunit is a unit test result format. Here we output a stream of
-    # test results, one per measurement.
-    # Each measurement is a successfull test if its overall uptime %age
-    # is higher than args.min_uptime
-    if args.subunitreport:
-        with SubunitContext(args.subunitreport) as output:
-            for measurement_name, measurement_data in \
-                    report['per_field_uptime_percent'].items():
-                for measure_name, uptime_percentage in \
-                        measurement_data.items():
-                    status = "fail"
-                    if uptime_percentage > args.min_uptime:
-                        status = "success"
-                    # Record test start
-                    output.status(
-                        test_id=measure_name,
-                        timestamp=first_ts
-                    )
-
-                    # Record end of test
-                    output.status(
-                        test_id=measure_name,
-                        # TODO(hughsaunders): Be more intelligent
-                        # about thresholds
-                        test_status=status,
-                        test_tags=None,
-                        runnable=False,
-                        file_name=measure_name,
-                        file_bytes=" {}% lower than threshold".format(
-                            uptime_percentage).encode('ascii'),
-                        timestamp=last_ts,
-                        eof=True,
-                        mime_type='text/plain; charset=UTF8'
-                    )
+    generate_reports(
+        data=stages,
+        max_downtime=100,
+        ymlfile=args.ymlreport,
+        subunitfile=args.subunitreport,
+    )
 
 
 if __name__ == "__main__":
     """ Args parser and logic router """
     logging.getLogger().setLevel(logging.INFO)
     parser = argparse.ArgumentParser(
-        description='Fetch maas_ metrics, and report downtime data.')
+        description="Fetch maas_ metrics, and report downtime data.")
     parser.add_argument("--ymlreport", help="Yaml report filename")
     parser.add_argument("--subunitreport", help="Subunit report filename")
     parser.add_argument(
-        "--min-uptime",
-        help="Minimum uptime required (percentage)",
-        default=1,
-        type=float)
+        "--max-downtime",
+        help="Maximum allowed downtime (percentage)",
+        default=99.0,
+        type=float,
+    )
+    parser.add_argument("--leapfrog-upgrade", action="store_true")
     arguments = parser.parse_args()
     try:
-        arguments.influx_ip = os.environ['INFLUX_IP']
+        arguments.influx_ip = os.environ["INFLUX_IP"]
     except KeyError:
         logging.error("Please set INFLUX_IP")
         sys.exit(1)
 
-    try:
-        arguments.influx_port = os.environ['INFLUX_PORT']
-    except KeyError:
-        logging.error("Please set INFLUX_PORT")
-        sys.exit(2)
+    arguments.influx_port = os.environ.get("INFLUX_PORT", "8086")
 
     try:
-        arguments.job_reference = os.environ['BUILD_TAG']
+        arguments.build_ref = os.environ["BUILD_TAG"]
     except KeyError:
         logging.error("Please set BUILD_TAG for its usage as job ref")
         sys.exit(3)
