54f8a128f1e7e4032a053f702d5fc4ff23773641
git-harry <git-harry@live.co.uk>
2017-09-02 21:20:07 +0100

    Ping loss reporting
    - adds support for Telegraf ping plugin
    - outputs more detailed breakdown of data
    - allows file locations to be overridden to allow report to be run on
      Jenkins log artefacts
    - add use of globbing to support changing filename versions

--- influx-reports/influx.py
+++ influx-reports/influx.py
@@ -2,8 +2,9 @@
 
 import argparse
 from collections import defaultdict
-from datetime import datetime, timedelta
 from copy import deepcopy
+from datetime import datetime, timedelta
+from glob import iglob
 import logging
 import os
 import sys
@@ -40,7 +41,7 @@ LEAPFROG_STAGES = [
                     },
                     {
                         "stage_name": "OSA-OPS/migrations.sh",
-                        "filename": "openstack-ansible-14.2.3-db.leap",
+                        "filename": "openstack-ansible-14.*-db.leap",
                     },
                     {
                         "stage_name": "OSA-OPS/redeploy.sh",
@@ -66,6 +67,10 @@ LEAPFROG_STAGES = [
 
 
 class InfluxTimestampParseException(Exception):
+    pass
+
+
+class MultipleFilenameMatches(Exception):
     pass
 
 
@@ -97,9 +102,9 @@ def generate_reports(data, max_downtime=100, ymlfile=None, subunitfile=None):
     if subunitfile:
         with SubunitContext(subunitfile) as output:
             for stage in data:
-                for svc_name, svc_data in stage["services_down"].items():
+                for svc_name, svc_data in stage["services_states"].items():
                     status = "fail"
-                    if svc_data["percentage"] < max_downtime:
+                    if svc_data["DOWN percentage"] < max_downtime:
                         status = "success"
                     # Record test start
                     output.status(
@@ -117,23 +122,29 @@ def generate_reports(data, max_downtime=100, ymlfile=None, subunitfile=None):
                         runnable=False,
                         file_name=svc_name,
                         file_bytes=" {}% higher than threshold".format(
-                            svc_data["percentage"]).encode("ascii"),
+                            svc_data["DOWN percentage"]).encode("ascii"),
                         timestamp=stage["completed"],
                         eof=True,
                         mime_type="text/plain; charset=UTF8"
                     )
 
 
-def get_mtime(filename):
-    leapfiledir = "/opt/rpc-leapfrog/leap42"
-    completefiledir = "/etc/openstack_deploy/upgrade-leap"
+def get_mtime(filename, leapfiledir, completefiledir):
     if filename.endswith(".complete"):
         directory = completefiledir
     elif filename.endswith(".leap"):
         directory = leapfiledir
     else:
         directory = "./"
-    path = os.path.join(directory, filename)
+
+    paths = iglob(os.path.join(directory, filename))
+    path = next(paths)
+    try:
+        next(paths)
+    except StopIteration:
+        pass
+    else:
+        raise MultipleFilenameMatches
     file_stats = os.stat(path)
 
     return datetime.utcfromtimestamp(file_stats.st_mtime).replace(
@@ -143,8 +154,8 @@ def get_mtime(filename):
 
 def get_downtime(client, build, start, end):
     measurements = [
-        "maas_glance", "maas_cinder", "maas_keystone",
-        "maas_heat", "maas_neutron", "maas_nova", "maas_horizon"
+        "maas_glance", "maas_cinder", "maas_keystone", "maas_heat",
+        "maas_neutron", "maas_nova", "maas_horizon", "ping",
     ]
     resolution = 60
 
@@ -152,11 +163,11 @@ def get_downtime(client, build, start, end):
     end_rounded = end.replace(second=0) + timedelta(minutes=1)
     max_downtime_rounded = (end_rounded - start_rounded).total_seconds()
 
-    service_downtime = defaultdict(lambda: max_downtime_rounded)
+    service_state = defaultdict(lambda: {"up": 0, "down": 0, "unknown": 0})
 
     for measurement in measurements:
         query = (
-            "select max(/.*_status/) "
+            "select max(/.*_status|percent_packet_loss/) "
             "from {measurement} "
             "where time > '{start:%Y-%m-%d %H:%M:%S}' "
             "and time < '{end:%Y-%m-%d %H:%M:%S}' "
@@ -179,18 +190,31 @@ def get_downtime(client, build, start, end):
         )
         for name, value in elements:
             key = str(name.replace("max_", "").replace("_status", ""))
+            if key == "percent_packet_loss":
+                value = 1 if value == 0 else value
             if value == 1:
-                service_downtime[key] -= resolution
+                service_state[key]["up"] += resolution
+            elif value == -1:
+                service_state[key]["unknown"] += resolution
             else:
-                # Ensure all services are added to the defaultdict
-                service_downtime[key]
+                service_state[key]["down"] += resolution
 
     return {
         svc: {
-            "hh:mm:ss": str(timedelta(seconds=secs)),
-            "percentage": round(100 * secs / max_downtime_rounded, 1),
+            "UP hh:mm:ss": str(timedelta(seconds=secs["up"])),
+            "UP percentage": round(
+                100 * secs["up"] / max_downtime_rounded, 1
+            ),
+            "DOWN hh:mm:ss": str(timedelta(seconds=secs["down"])),
+            "DOWN percentage": round(
+                100 * secs["down"] / max_downtime_rounded, 1
+            ),
+            "UNKNOWN hh:mm:ss": str(timedelta(seconds=secs["unknown"])),
+            "UNKNOWN percentage": round(
+                100 * secs["unknown"] / max_downtime_rounded, 1
+            ),
         }
-        for svc, secs in service_downtime.items()
+        for svc, secs in service_state.items()
     }
 
 
@@ -216,27 +240,38 @@ def return_time(client, query, delta_seconds=0):
     return fpt
 
 
-def add_time(client, build, stages, started):
+def add_time(client, build, stages, started, leapfiledir, completefiledir):
     for stage in stages:
-        completed = get_mtime(stage["filename"])
+        completed = get_mtime(stage["filename"], leapfiledir, completefiledir)
         del stage["filename"]
         duration = completed - started
         stage["started"] = started
         stage["completed"] = completed
         stage["duration"] = str(duration)
-        stage["services_down"] = get_downtime(
+        stage["services_states"] = get_downtime(
             client, build, started, completed
         )
-        add_time(client, build, stage.get("stages", []), started)
+        add_time(
+            client,
+            build,
+            stage.get("stages", []),
+            started,
+            leapfiledir,
+            completefiledir,
+        )
         started = completed
 
 
-def get_build_data(client, build, leapfrog=False):
+def get_build_data(
+    client, build, leapfrog=False, leapfiledir=None, completefiledir=None
+        ):
     if leapfrog:
         # This is the first marker file created and so approximately the start
-        start_time = get_mtime("clone.complete")
+        start_time = get_mtime("clone.complete", leapfiledir, completefiledir)
         stages = deepcopy(LEAPFROG_STAGES)
-        add_time(client, build, stages, start_time)
+        add_time(
+            client, build, stages, start_time, leapfiledir, completefiledir
+        )
     else:
         # First find the first and last timestamp from telegraf.
         # This way querying maas_* data will always
@@ -255,13 +290,13 @@ def get_build_data(client, build, leapfrog=False):
         completed = return_time(
             client, find_last_timestamp_query, delta_seconds=-5
         )
-        services_down = get_downtime(
+        services_states = get_downtime(
             client, build, started, completed
         )
         stages = [
             {
                 "stage_name": "Upgrade",
-                "services_down": services_down,
+                "services_states": services_states,
                 "stages": [],
                 "started": started,
                 "completed": completed,
@@ -277,7 +312,11 @@ def main(args):
         args.influx_ip, args.influx_port, database="telegraf"
     )
     stages = get_build_data(
-        client, args.build_ref, leapfrog=args.leapfrog_upgrade
+        client,
+        args.build_ref,
+        leapfrog=args.leapfrog_upgrade,
+        leapfiledir=args.leapfiledir,
+        completefiledir=args.completefiledir,
     )
     generate_reports(
         data=stages,
@@ -301,6 +340,14 @@ if __name__ == "__main__":
         type=float,
     )
     parser.add_argument("--leapfrog-upgrade", action="store_true")
+    parser.add_argument(
+        "--leapfiledir",
+        default="/opt/rpc-leapfrog/leap42",
+    )
+    parser.add_argument(
+        "--completefiledir",
+        default="/etc/openstack_deploy/upgrade-leap",
+    )
     arguments = parser.parse_args()
     try:
         arguments.influx_ip = os.environ["INFLUX_IP"]
