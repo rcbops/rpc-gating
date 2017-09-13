950aa82e0d6765f61413d1d393ef985e4f8ee64a
git-harry <git-harry@live.co.uk>
2017-09-13 20:27:52 +0100

    LA-436 Re-add Python path in influx-report.sh
    The use of python in the virtual env `.venv` was incorrectly removed.
    This change adds it back so the script `influx.py` is run with the
    required dependencies.

--- influx-reports/influx-report.sh
+++ influx-reports/influx-report.sh
@@ -5,4 +5,4 @@ INFLUX_DOWNTIME_FOLDER="$(dirname "${BASH_SOURCE[0]}")"
 ADDITIONAL_ARGS="$*"
 
 mkdir -p "${REPORTS_FOLDER}"
-$INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-downtime.yml --subunitreport $REPORTS_FOLDER/influx-downtime.bin $ADDITIONAL_ARGS
+$PYTHON_PATH $INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-downtime.yml --subunitreport $REPORTS_FOLDER/influx-downtime.bin $ADDITIONAL_ARGS
