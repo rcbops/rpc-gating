53882fd4133f33e3ebe1b3b74c973847d342636a
Matt Thompson <mattt@defunct.ca>
2017-10-11 15:09:59 -0400

    Test

--- job_dsl/merge_trigger_jjb.groovy
+++ job_dsl/merge_trigger_jjb.groovy
@@ -1,11 +1,11 @@
 library "rpc-gating@${RPC_GATING_BRANCH}"
 common.shared_slave(){
   stage('Run Jenkins Job Builder') {
-    def scmVars = checkout scm
-    if (scmVars.GIT_BRANCH == "master" || scmVars.GIT_BRANCH == null){
-      build job: "Jenkins-Job-Builder"
-    } else {
-      print ("Not running JJB as job was triggered by a push to branch ${env.GIT_BRANCH}, and not master")
-    }
+    print("Triggering JJB") 
+    //if (scmVars.GIT_BRANCH == "master" || scmVars.GIT_BRANCH == null){
+    //  build job: "Jenkins-Job-Builder"
+    //} else {
+    //  print ("Not running JJB as job was triggered by a push to branch ${env.GIT_BRANCH}, and not master")
+    //}
   } // stage
 } // node
