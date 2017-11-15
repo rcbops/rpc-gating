b97208ef606aba4425890fec5327ad228efede41
git-harry <git-harry@live.co.uk>
2017-11-15 18:34:19 +0000

    RE-1075 Extend standard jobs to support Docker
    To enable the standard jobs to use Docker containers or OpenStack
    compute instances, this change adds a new function,
    `standard_job_slave`, which manages the slave set-up. The type of slave
    a build uses is determined by the parameter `SLAVE_TYPE`, the default
    set in the jobs is `"instance"`, to use a Docker container set it to
    `"container"`.

--- pipeline_steps/common.groovy
+++ pipeline_steps/common.groovy
@@ -682,6 +682,27 @@ void internal_slave(body){
   use_node("CentOS", body)
 }
 
+void standard_job_slave(String slave_type, Closure body){
+  timeout(time: 6, unit: 'HOURS'){
+    shared_slave(){
+      if (slave_type == "instance"){
+        pubcloud.runonpubcloud(){
+          body()
+        }
+      } else if (slave_type == "container"){
+        dir("rpc-gating"){
+          container = docker.build env.BUILD_TAG.toLowerCase()
+        }
+        container.inside {
+          body()
+        }
+      } else {
+        throw new Exception("slave_type '$slave_type' is not supported.")
+      }
+    }
+  }
+}
+
 // Build an array suitable for passing to withCredentials
 // from a space or comma separated list of credential IDs.
 @NonCPS
