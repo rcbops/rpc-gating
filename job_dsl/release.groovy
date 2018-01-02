41166c266671ee843adc3f9f6ebb53bf05c83f6b
Hugh Saunders <hugh@wherenow.org>
2018-01-02 11:43:00 +0000

    RE-1191 Add common.globalwraps
    This function is called to wrap every job.
    This allows us to add and remove global wrappers in a single location.
    Currently the global wrappers are:
      * timestamps
      * timeout (10 hours), jobs can still set their own shorter timeouts,
        This timeout was chosen to be longer than any existing timeout.
      * Logfile size checker: Aborts build if console log exceeds 200MB.

--- job_dsl/release.groovy
+++ job_dsl/release.groovy
@@ -1,40 +1,42 @@
 library "rpc-gating@${RPC_GATING_BRANCH}"
-common.shared_slave(){
+common.globalWraps(){
+  common.shared_slave(){
 
-  stage("Configure Git"){
-    common.configure_git()
-  }
-
-  stage("Release"){
-    // If this is a PR test, then we need to set some
-    // of the environment variables automatically as
-    // they will not be provided by a human.
-    if ( env.ghprbPullId != null ) {
-      List source_repo = env.ghprbAuthorRepoGitUrl.split("/")
-      env.ORG = source_repo[-2]
-      env.REPO = source_repo[-1].tokenize(".")[0]
-      env.RC_BRANCH = env.ghprbSourceBranch
+    stage("Configure Git"){
+      common.configure_git()
     }
-    withCredentials([
-      string(
-        credentialsId: 'rpc-jenkins-svc-github-pat',
-        variable: 'PAT'
-      ),
-      string(
-        credentialsId: 'mailgun_mattt_endpoint',
-        variable: 'MAILGUN_ENDPOINT'
-      ),
-      string(
-        credentialsId: 'mailgun_mattt_api_key',
-        variable: 'MAILGUN_API_KEY'
-      )
-    ]){
-      sshagent (credentials:['rpc-jenkins-svc-github-ssh-key']){
-        sh """#!/bin/bash -xe
-          set +x; . .venv/bin/activate; set -x
-          ${env.COMMAND}
-        """
-      } // sshagent
-    } // withCredentials
-  } // stage
-}
+
+    stage("Release"){
+      // If this is a PR test, then we need to set some
+      // of the environment variables automatically as
+      // they will not be provided by a human.
+      if ( env.ghprbPullId != null ) {
+        List source_repo = env.ghprbAuthorRepoGitUrl.split("/")
+        env.ORG = source_repo[-2]
+        env.REPO = source_repo[-1].tokenize(".")[0]
+        env.RC_BRANCH = env.ghprbSourceBranch
+      }
+      withCredentials([
+        string(
+          credentialsId: 'rpc-jenkins-svc-github-pat',
+          variable: 'PAT'
+        ),
+        string(
+          credentialsId: 'mailgun_mattt_endpoint',
+          variable: 'MAILGUN_ENDPOINT'
+        ),
+        string(
+          credentialsId: 'mailgun_mattt_api_key',
+          variable: 'MAILGUN_API_KEY'
+        )
+      ]){
+        sshagent (credentials:['rpc-jenkins-svc-github-ssh-key']){
+          sh """#!/bin/bash -xe
+            set +x; . .venv/bin/activate; set -x
+            ${env.COMMAND}
+          """
+        } // sshagent
+      } // withCredentials
+    } // stage
+  } // shared_slave
+} // globalWraps
