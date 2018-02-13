7baebf2623ba1356bc175ec5ee3e5807448b046c
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-02-13 13:57:18 +0000

    RE-1313 Implement validation of timed trigger/CRON values
    In order to validate whether a CRON value or timed trigger
    value is valid, we implement a check in the lint test to
    verify that it will work, and to verify that standard jobs
    use a more limited set.

--- scripts/lint_jjb.py
+++ scripts/lint_jjb.py
@@ -6,12 +6,19 @@
 #   * Retention Policy
 
 import argparse
+import jmespath
 import os
 import re
 import sys
 import yaml
 
-import jmespath
+# The python-crontab library thinks that the following
+# conditions are true, so we use the croniter library
+# instead as it is better at validating input.
+# crontab.CronSlices.is_valid("")
+# crontab.CronSlices.is_valid(None)
+# crontab.CronSlices.is_valid(0)
+from croniter import croniter
 
 
 def parse_args():
@@ -56,6 +63,8 @@ def parse_jjb_file(in_dir, in_file):
             for job in item["project"]["jobs"]:
                 if parse_job_name(job, filename):
                     _rc = 1
+            if check_timed_trigger(item["project"], filename):
+                _rc = 1
 
         if "job-template" in item:
             # check job-template name
@@ -63,12 +72,16 @@ def parse_jjb_file(in_dir, in_file):
                 _rc = 1
             if check_retention(item['job-template'], filename):
                 _rc = 1
+            if check_timed_trigger(item['job-template'], filename):
+                _rc = 1
 
         if "job" in item:
             # check individual job
             if parse_job_name(item["job"]["name"], filename):
                 _rc = 1
             if check_retention(item['job'], filename):
+                _rc = 1
+            if check_timed_trigger(item['job'], filename):
                 _rc = 1
     return _rc
 
@@ -151,6 +164,149 @@ def check_retention(job, in_file):
             break
     else:
         sys.stderr.write(error_message)
+
+    return return_value
+
+
+# Ensure all jobs have a valid timed triggers entry
+# Structure:
+#   job:
+#     triggers:
+#      - timed: "@daily"
+def check_timed_trigger_value(trigger):
+
+    # Non-standard CRON values allowed by jenkins
+    # Reference:
+    # https://en.wikipedia.org/wiki/Cron#Nonstandard_predefined_scheduling_definitions
+    # http://www.scmgalaxy.com/tutorials/setting-up-the-cron-jobs-in-jenkins-using-build-periodically-scheduling-the-jenins-job/
+    allowed_non_standard_values = [
+        "@yearly", "@annually", "@monthly", "@weekly", "@daily",
+        "@midnight", "@hourly"
+    ]
+
+    # If the trigger provided is a NoneType,
+    # then the job has no value, so pass it.
+    if trigger is None:
+        return_value = 0
+    # If the trigger provided is not a string,
+    # then it is not valid.
+    elif not isinstance(trigger, str):
+        return_value = 1
+    # If the CRON macro is used, we do not need
+    # to validate it.
+    elif trigger == "{CRON}":
+        return_value = 0
+    # If non-standard predefined schedule definitions
+    # are used, pass them.
+    elif trigger in allowed_non_standard_values:
+        return_value = 0
+    else:
+        # Jenkins supports the use of hashes, denoted by the symbol H,
+        # in the trigger to allow the load to be spread on the system.
+        # The croniter library does not know how to validate them, so
+        # we replace them.
+        # Valid inputs and what they should output after replacement:
+        # 1. "H * * * *" > "* * * * *"
+        # 2. "H(0-29)/10 * * * *" > "0-29/10 * * * *"
+        # 3. "H/10 H(10-11) * H *" > "*/10 10-11 * * *"
+
+        # handle the H(..) pattern first
+        _trigger = re.sub(r'H\((\d+-\d+)\)', r'\1', trigger)
+
+        # then handle the H pattern
+        _trigger = re.sub(r'H', r'*', _trigger)
+
+        if croniter.is_valid(_trigger):
+            return_value = 0
+        else:
+            return_value = 1
+
+    return return_value
+
+
+# Ensure all jobs have a valid timed triggers entry
+# Structure:
+#   job:
+#     triggers:
+#      - timed: "@daily"
+#      - timed: "{CRON}"
+def check_timed_trigger_list(data, in_file):
+
+    timed_trigger_list = jmespath.search('triggers[*].timed', data)
+
+    return_value = 0
+    for trigger in timed_trigger_list or []:
+
+        error_message = ("{f}/{n}: Valid timer trigger not found."
+                         " trigger: {t}\n"
+                         .format(n=data['name'], f=in_file,
+                                 t=trigger))
+
+        if check_timed_trigger_value(trigger) == 1:
+            sys.stderr.write(error_message)
+            return_value = 1
+
+    return return_value
+
+
+def check_timed_trigger_cron(data, in_file):
+    error_message = ("{f}/{n}: Valid CRON value not found."
+                     " CRON: {t}\n"
+                     .format(n=data['name'], f=in_file,
+                             t=data.get('CRON')))
+
+    cron_value = data.get('CRON')
+
+    return_value = 0
+    if check_timed_trigger_value(cron_value) == 1:
+        sys.stderr.write(error_message)
+        return_value = 1
+
+    return return_value
+
+
+def check_timed_trigger_cron_std(data, in_file):
+
+    standard_pm_template = (
+        'PM_{repo_name}-{branch}-{image}-{scenario}-{action}')
+
+    allowed_values = [
+        "@daily", "@weekly", "@monthly"
+    ]
+
+    error_message = ("{f}/{n}: Valid CRON value not found"
+                     " for standard job. Only allowed values"
+                     " are {a}. Found: {t}\n"
+                     .format(n=data['name'], f=in_file,
+                             a=allowed_values, t=data.get('CRON')))
+
+    jobs_list = data.get('jobs') or []
+    cron_value = data.get('CRON')
+
+    # The variable cron_value must have
+    # a value to be tested, otherwise
+    # there is no CRON parameter in the
+    # job so we pass the test.
+    if (cron_value and standard_pm_template in jobs_list):
+
+        if cron_value in allowed_values:
+            return 0
+        else:
+            sys.stderr.write(error_message)
+            return 1
+
+    else:
+        return 0
+
+
+def check_timed_trigger(data, in_file):
+
+    if (check_timed_trigger_list(data, in_file) == 1 or
+            check_timed_trigger_cron(data, in_file) == 1 or
+            check_timed_trigger_cron_std(data, in_file) == 1):
+        return_value = 1
+    else:
+        return_value = 0
 
     return return_value
 
