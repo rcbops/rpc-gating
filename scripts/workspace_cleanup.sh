cc46596a5bbc4cceb6f6dd5176fdfd62159fb8ea
Hugh Saunders <hugh@wherenow.org>
2018-05-02 13:54:22 +0100

    RE-1452 Ensure master is included in cleanup
    Master now runs build summary in docker. As docker is being used,
    its important that docker cleanup is run regularly. However a bug
    in the periodic cleanup job means that cleanup is not running on
    master.
    
    This commit ensures that cleanup does indeed run on the master.

--- scripts/workspace_cleanup.sh
+++ scripts/workspace_cleanup.sh
@@ -3,5 +3,5 @@
 echo "Removing Workspaces that haven't been modified in the last 2 days"
 set -xe
 cd /var/lib/jenkins/workspace \
-  && find . -maxdepth 1 -ctime +2 \
+  && find . -maxdepth 1 -mindepth 1 -ctime +2 \
     |while read old; do rm -rf "$old"; done
