e17beb88712466f014fb9ceb8dca4261236545c7
David Deal <david.deal@rackspace.com>
2018-05-10 09:15:01 -0700

    RE-1477 Docker Cleanup Fixes
    - Added sudo for docker commands to allow Jenkins Master cleanup to work.

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -3,10 +3,10 @@
 set -xe
 echo "Remove exited containers"
 #https://docs.docker.com/engine/reference/commandline/ps/#examples
-(docker ps -a -q -f "status=exited"; docker ps -a -q -f "status=dead")| while read cid; do docker rm $cid||:; done
+sudo docker ps -a -q --filter "status=exited" --filter "status=dead" | while read cid; do sudo docker rm $cid||:; done
 
 echo "Remove old Images"
 # Remove any images that have been created more than a day ago, unless it is
 # the latest ubuntu image.
-diid=$(docker images -a | grep -vP "^ubuntu.*latest" | egrep "[0-9]{2,} (hour|day|week)s?" | awk '{print $3}')
-for dimage in $diid; do docker rmi -f ${dimage}||:; done
+diid=$(sudo docker images -a | grep -vP "^ubuntu.*latest" | egrep "[0-9]{2,} (hour|day|week)s?" | awk '{print $3}')
+for dimage in $diid; do sudo docker rmi -f ${dimage}||:; done
