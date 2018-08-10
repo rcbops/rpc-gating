de46f9fe15460285b432e2844084b596e12aeba1
Hugh Saunders <hugh@wherenow.org>
2018-08-10 13:01:36 +0100

    Build Summary: Update cachequery for json data
    Cache query contains some routines for inspecting the cached
    data used by build summary, previously it was the pickle dump
    and now its json.

--- scripts/build_summary/cachequery.py
+++ scripts/build_summary/cachequery.py
@@ -1,23 +1,36 @@
-import click
-import pickle
+import json
+import dateutil.parser
+
+# These functions are not called anywhere directly, but are useful for
+# loading into an interactive environment for inspecting a json build
+# data file.
 
 
-@click.group()
-def cli():
-    pass
+# load build data
+def loadbd(filename):
+    data = json.load(open(filename))
+    builds = data['builds']
+    failures = data['failures']
+
+    # transform the uuids used to reference objects in json, back into
+    # object references
+    # also convert iso 8601 timestamps into date objects.
+    for b in builds.values():
+        b['failures'] = [failures[uuid] for uuid in b['failures']]
+        b['timestamp'] = dateutil.parser.parse(b['timestamp'])
+    for f in failures.values():
+        f['build'] = builds[f['build']]
+    return (data, builds.values(), failures.values())
 
 
-@cli.command()
-@click.option('--cache-file', default='test-cache')
-@click.option('--query')
-def query(cache_file, query):
-    with open(cache_file, 'rb') as f:
-        key, criteria = query.split('=')
-        buildobjs = pickle.load(f)
-        for name, build in buildobjs.items():
-            item = getattr(build, key, '')
-            if criteria in item:
-                print(build, item)
-
-
-cli()
+# get builds and failures for one day
+def objectsForDay(data, year, month, day):
+    def istd(b):
+        return (b['timestamp'].day == day
+                and b['timestamp'].month == month
+                and b['timestamp'].year == year)
+    builds = data['builds'].values()
+    failures = data['failures'].values()
+    daybuilds = [b for b in builds if istd(b)]
+    dayfailures = [f for f in failures if istd(f['build'])]
+    return (daybuilds, dayfailures)
