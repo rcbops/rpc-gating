2bcdbe8ef46b3ccdc8554188aced883c240f4734
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2019-01-24 11:19:09 +0000

    Implement a common method for rpc-gating runtime requirements
    Currently there are multiple methods for preparing a host for
    tests:
    
    1. In the jenkins nodepool plugin configuration, there is a script
       which executes on all target nodes to prepare the node for use.
       This script is used to add the jenkins user, add the ssh keys,
       add the JDK, etc. This is used for preparing OnMetal/Ironic
       hosts where we do not control the image.
    
    2. In the setup_openstack_instances playbook, we use the raw Ansible
       module to add python to the target host so that Ansible is able
       to execute other tasks and modules on that host.
       This is used for preparing VM's where we do not control the image.
    
    3. In the allocate_pubcloud playbook we use the raw and script
       modules to ensure that the apt sources are configured, apt debug
       logging is enabled, and that python and python-yaml are installed
       on the target host.
       This is used for preparing VM's where we do not control the image.
    
    In this patch, we combine these all into a single script which is
    used as userdata provided to cloud-init [1], which is present in all
    the images for public cloud and Phobos.
    
    In order to use this for nodepool, we also need to update the nodepool
    SHA we use to include the support for userdata. In the nodepool config
    we unfortunately cannot import the file directly into the config, so
    we use another method of getting the same file for the same SHA.
    
    As part of this, we also make the following minor adjustments:
    
    1. In the allocate_pubcloud playbook we use the Ansible playbook_dir
       magic variable, rather than refer to the WORKSPACE environment
       variable. This makes the playbook executable for testing without
       having to set the WORKSPACE environment variable.
    
    2. In the user_data_publoud script, we remove the unattended upgrade
       disabling. Now that all RPC-O tests make use of nodepool images
       the issue which brought that content there is no longer relevant.
    
    [1] https://cloud-init.io
    JIRA: RE-1523

--- scripts/user_data_pubcloud.sh
+++ scripts/user_data_pubcloud.sh
@@ -1,23 +1,123 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# This file is used when creating public
-# cloud instances.
-# See playbooks/allocate_pubcloud.yml
+# This script is used as a user data file when building
+# machines which use images that are not managed by
+# nodepool. See https://cloud-init.io for more information
+# about how cloud-init uses user-data files.
+#
+# This script tries to ensure that the machine is prepared
+# in a consistent way and it has all the required packages
+# installed on it for jenkins/ansible to access it. Any
+# further preparation should be done using jenkins/ansible.
 
-source /etc/lsb-release
-
-# Delete configuration which enables automatic upgrades
-# to prevent the instance packages being upgraded before
-# the correct apt repositories have been configured.
-# ref: RE-458 / RE-473
-if [[ "${DISTRIB_CODENAME}" == "trusty" ]]; then
-    # The 'unattended-upgrades' package cannot
-    # be purged on trusty as it is a dependency
-    # for cloud-init. So instead we just remove
-    # the configuration.
-    rm -f /etc/apt/apt.conf.d/*unattended-upgrades
+# Get the distribution name
+echo -e "\n### Getting the distribution name ###\n"
+if [[ -e /etc/lsb-release ]]; then
+  source /etc/lsb-release
+  DISTRO_RELEASE=${DISTRIB_CODENAME}
+elif [[ -e /etc/os-release ]]; then
+  source /etc/os-release
+  DISTRO_RELEASE=${UBUNTU_CODENAME}
 else
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get purge -y unattended-upgrades
+  echo "Unable to determine distribution due to missing lsb/os-release files."
+  exit 1
 fi
+
+# Save a backup of the original apt sources
+if [[ ! -f "/etc/apt/sources.list.original" ]]; then
+  echo -e "\n### Saving a backup of the original apt sources ###\n"
+  mv /etc/apt/sources.list /etc/apt/sources.list.original
+  echo -e "\n###Rewriting the apt sources ###\n"
+  DISTRO_MIRROR="http://mirror.rackspace.com/ubuntu"
+  DISTRO_COMPONENTS="main,universe"
+  cat << EOF >/etc/apt/sources.list
+deb ${DISTRO_MIRROR} ${DISTRO_RELEASE} ${DISTRO_COMPONENTS//,/ }
+deb ${DISTRO_MIRROR} ${DISTRO_RELEASE}-updates ${DISTRO_COMPONENTS//,/ }
+deb ${DISTRO_MIRROR} ${DISTRO_RELEASE}-backports ${DISTRO_COMPONENTS//,/ }
+deb ${DISTRO_MIRROR} ${DISTRO_RELEASE}-security ${DISTRO_COMPONENTS//,/ }
+EOF
+fi
+
+# Enable debug logging for apt to make diagnosis of apt failures easier
+if [[ ! -f "/etc/apt/apt.conf.d/99debug" ]]; then
+  echo -e "\n### Enabling apt debug logging ###\n"
+  echo 'Debug::Acquire::http "true";' > /etc/apt/apt.conf.d/99debug
+fi
+
+# Add jenkins user and group
+JENKINS_HOME="/var/lib/jenkins"
+if ! getent group jenkins &> /dev/null; then
+  echo -e "\n### Adding the jenkins group ###\n"
+  groupadd jenkins
+fi
+if ! getent passwd jenkins &> /dev/null; then
+  echo -e "\n### Adding the jenkins user ###\n"
+  useradd --gid jenkins \
+          --shell /bin/bash \
+          --home-dir ${JENKINS_HOME} \
+          --create-home jenkins
+fi
+
+# Fetch the rcbops public keys and add them to authorized_keys
+echo -e "\n### Fetching the rcbops public keys ###\n"
+SSH_PUBLIC_KEYS_URL="https://raw.githubusercontent.com/rcbops/rpc-gating/master/keys/rcb.keys"
+curl --silent \
+     --show-error \
+     --fail \
+     --connect-timeout 5 \
+     --retry 3 \
+     ${SSH_PUBLIC_KEYS_URL} > /tmp/ssh-public-keys
+
+echo -e "\n### Configuring authorized_keys for jenkins and root ###\n"
+for usr_home in /root ${JENKINS_HOME}; do
+  mkdir -p ${usr_home}/.ssh
+  chmod 700 ${usr_home}/.ssh
+  cat /tmp/ssh-public-keys >> ${usr_home}/.ssh/authorized_keys
+  chmod 644 ${usr_home}/.ssh/authorized_keys
+done
+
+# Configure sudoers for the jenkins user
+echo -e "\n### Configuring sudoers for jenkins ###\n"
+cat > /etc/sudoers.d/jenkins << EOF
+jenkins ALL=(ALL) NOPASSWD:ALL
+EOF
+chmod 0440 /etc/sudoers.d/jenkins
+
+# Ensure everything has the right owner
+echo -e "\n### Ensuring jenkins owns all jenkins home files ###\n"
+chown -R jenkins:jenkins ${JENKINS_HOME}
+
+# For Ubuntu Trusty add the openjdk PPA for access
+# to the openjdk8 packages
+echo -e "\n### Adding openjdk PPA for Ubuntu Trusty hosts ###\n"
+if [[ "${DISTRO_RELEASE}" == "trusty" ]]; then
+  add-apt-repository -y ppa:openjdk-r/ppa
+fi
+
+# Prepare the list of packages to install
+echo -e "\n### Preparing a list of packages to install ###\n"
+pkgs_required=""
+pkgs_to_install=""
+pkgs_required+=" openjdk-8-jre-headless" # for the jenkins agent
+pkgs_required+=" python-minimal" # required by ansible
+pkgs_required+=" python-yaml" # required by ansible
+
+for pkg in ${pkgs_required}; do
+  if ! dpkg-query --list ${pkg} &>/dev/null; then
+    pkgs_to_install+=" ${pkg}"
+  fi
+done
+
+# Update the apt cache and installing the required packages
+if [[ "${pkgs_to_install}" != "" ]]; then
+  echo -e "\n### Updating the apt cache ###\n"
+  apt-get update
+  echo -e "\n### Installing the required packages ###\n"
+  export DEBIAN_FRONTEND="noninteractive"
+  apt-get install -y ${pkgs_to_install}
+fi
+
+# Regardless of the last command's return code, ensure that the script exits
+# with RC=0, otherwise cloud-init may fail the boot.
+echo -e "\n### Host preparation complete ###\n"
 exit 0
