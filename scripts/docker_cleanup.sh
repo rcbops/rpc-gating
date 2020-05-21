3ed34aa5d35e97141ab3720f6f56ce2eb7c5cbbb
Shannon Mitchell <shannon.mitchell@rackspace.com>
2020-05-21 17:09:45 -0500

    RS is removing the phobos and kronos vpns. It looks like most if it can be removed as it is not used.  The periodic_cleanup does use an image built and uploaded to a kronos kube cluster. I have commented out the docker images cleanup portion of the docker cleanup script so we have an image for use by the cleanup script on the slaves. The build summary was being worked on during the org changes, so that can be safely removed.

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -5,8 +5,10 @@ echo "Remove exited containers"
 #https://docs.docker.com/engine/reference/commandline/ps/#examples
 sudo docker ps -a -q --filter "status=exited" --filter "status=dead" | while read cid; do sudo docker rm $cid||:; done
 
-echo "Remove old Images"
+
+# Commenting this out.  We no longer have an intrnal docker repo for these, so lets keep existing images until we repoace jenkins.
+# echo "Remove old Images"
 # Remove any images that have been created more than a day ago, unless it is
 # the latest ubuntu image.
-diid=$(sudo docker images -a | grep -vP "^ubuntu.*latest" | egrep "[0-9]{2,} (hour|day|week)s?" | awk '{print $3}')
-for dimage in $diid; do sudo docker rmi -f ${dimage}||:; done
+# diid=$(sudo docker images -a | grep -vP "^ubuntu.*latest" | egrep "[0-9]{2,} (hour|day|week)s?" | awk '{print $3}')
+# for dimage in $diid; do sudo docker rmi -f ${dimage}||:; done
